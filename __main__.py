09ac0ff5142edaca90d83833ba349c18d67243e2
Vusalldidaa <95521178+Vusalldidaa@users.noreply.github.com>
2022-01-15 11:57:23 +0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -8,12 +8,12 @@ from SDSongBot import SDbot as app
 from SDSongBot import LOGGER
 
 pm_start_text = """
-👋 Hey [{}](tg://user?id={}), **I'm Song Downloader Bot**
-**Now send me the song name you want to download**
+👋 Salam! [{}](tg://user?id={}), **Mən Mahnı Yükləyən Botam**
+**Mənə İstədiyin Mahnının Adın Göndər Məsələn **
      
-Syntax : ```/dsong Faded```
+Əmir: : ```/dsong Taladro Kendine İyi Bak```
       
-Powerd By @SDBotsz 🔥
+🇳🇴 Norway Chat @NorwayChat 🔥
 """
 
 @app.on_message(filters.command("start"))
@@ -26,10 +26,10 @@ async def start(client, message):
             [
                 [
                      InlineKeyboardButton(
-                        text="Channel 🙋‍♀️", url="https://t.me/SDBOTs_Inifinity"
+                        text="Kanal 🙋‍♀️", url="https://t.me/NorwayArea"
                     ),
                     InlineKeyboardButton(
-                        text="Dev 🔥", url="https://t.me/Itz_Sadew"
+                        text="👨‍💻 Sahibim", url="https://t.me/Vusaldeveloper"
                     )
                 ]
             ]
